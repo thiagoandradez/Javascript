64f70fa100deba9a178a768ebf801dbd91c47f5b
Thiago de Santana <168003660+thiagoandradez@users.noreply.github.com>
2024-10-25 15:44:39 -0300

    uptade new exercicies

---
+++ exercicios/exercicio008.js
@@ -0,0 +1,10 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+else {
+    console.log(`Você está dentro da velocidade, diriga sempre com segurança!`)
+}
---
+++ exercicios/exercicio009.js
@@ -0,0 +1,11 @@
+var pais = 'EUA'
+
+console.log(`Voce vive em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Brasileiro!')
+}
+
+else { 
+    console.log('Estrangeiro!')
+}
